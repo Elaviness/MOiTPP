8aef02733fae5c48db2444838c3a8e53cb517cff
Elaviness <48585255+Elaviness@users.noreply.github.com>
2020-10-22 22:22:17 +0300

    init commit

---
+++ test.py
@@ -0,0 +1,24 @@
+import collections
+
+med = []
+
+corps = collections.defaultdict(list)
+n = int(input('Введите количество предприятий: '))
+
+for i in range(n):
+    corp_name = input(str(i+1) + "-я компания: ")
+
+    for j in range(4):
+        quarter = int(input(f'Прибыль за {j+1}-й квартал: '))
+        corps[corp_name].append(quarter)
+
+    corps[corp_name].append(sum(corps[corp_name]) / 4)
+    med.append(corps[corp_name][4])
+
+print(f'Средняя прибыль за год для всех предприятий равна {sum(med) / n}')
+
+for k, v in corps.items():
+    if v[-1] < sum(med) / n:
+        print(f'У компании {k} средняя прибыль ниже среднего')
+    if v[-1] > sum(med) / n:
+        print(f'У компании {k} средняя прибыль ниже среднего')
